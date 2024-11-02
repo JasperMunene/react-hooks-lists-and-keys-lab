03bf75854ad658b860b0161ae94b17f63910551f
JasperMunene <jaspermunene2022@gmail.com>
2024-11-02 07:35:28 +0300

    Complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,16 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const navLinks = links.map((navlink) => {
+    return <a key={navlink} href={`#${navlink}`}>{navlink}</a>;
+  });
+
+  return (
+    <nav>
+      {/* display an <a> tag for each link here */}
+      {navLinks}
+    </nav>
+  );
 }
 
 export default NavBar;
